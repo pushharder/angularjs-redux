e26bc4f8ce59a2af71830c4634872dcc97e9ca5d
pushharder <alex.milovanov.mb@gmail.com>
2019-04-02 20:39:19 +0300

    add some css

--- src/app/main.module.js
+++ src/app/main.module.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import ngRedux from 'ng-redux';
 import mainComponent from './main.component';
 import toDoModule from './toDo/toDo.module';
+import './main.css';
 
 export default angular
 	.module('myAppModule', [ngRedux, toDoModule])
--- src/app/toDo/task/task.module.js
+++ src/app/toDo/task/task.module.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
 import taskComponent from './task.component';
+import styles from './task.css';
 
 export default angular.module('taskModule', []).component('taskComponent', taskComponent).name;
